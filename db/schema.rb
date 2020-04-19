1a729d6e5d0fa3c78d4b31a6293b5aad4e1040c6
Rex Ye <rxye1991@gmail.com>
2020-04-19 13:26:10 -0400

    addedpassworddigest

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_04_19_171748) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
