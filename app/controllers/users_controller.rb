ce1f9aa33c921f4d3ac38248a733120dab3a275e
Rex Ye <rxye1991@gmail.com>
2020-04-19 14:33:40 -0400

    renderUserobjectaftercreated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
     
     def create
-        byebug
+        user = User.create(new_user_params)
+        if user.valid?
+            render json: user, :except =>  [:password_digest, :created_at, :updated_at]
+        else
+            render json: {errors: @user.errors.full_messages}
+        end
     end
-    
+
+
+
+    private
+    def new_user_params
+        params.permit(:first_name, :last_name, :username, :password)
+    end
 end
