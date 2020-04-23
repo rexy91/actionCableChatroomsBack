f22e9356522a74f87ae10444eb5cf72b295067fa
Rex Ye <rxye1991@gmail.com>
2020-04-23 17:31:14 -0400

    seededThreeRoomsandsomemessages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Create three rooms:
+rex = User.create!({username:'rexye', password:'woaicheng1'})
+jo = User.create!({username:'jojo', password:'jojojo'})
+
+cats = Room.create(name: 'Cat Lovers', description:'This chatroom is for anyone who loves cats...')
+dogs = Room.create(name: 'Dog Lovers', description:'This chatroom is for anyone who loves dogs.')
+programmers = Room.create(name: 'Programmers', description:'This chatroom is for programmers.')
+
+Message.create(user:rex, room: programmers, content:'I am Rex, a JavaScript lover.')
+Message.create(user:jo, room:programmers, content:'I am jo, a Ruby lover.')
+
